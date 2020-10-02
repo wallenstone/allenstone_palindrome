2064d4ed854a2db7b1f7e6bd120e46e10145a780
Allen Stone <w.allen.stone@gmail.com>
2020-10-02 15:57:19 -0500

    Finish working and refactored palindrome method

--- lib/allenstone_palindrome.rb
+++ lib/allenstone_palindrome.rb
@@ -1,6 +1,20 @@
 require "allenstone_palindrome/version"
 
-module AllenstonePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+	# Returns true for a palindrome, false otherwise.
+	def palindrome?
+		processed_content == processed_content.reverse
+	end
+
+	# Returns the letters in the string.
+	def letters
+		self.chars.select { |c| c.match(/[a-z]/i) }.join
+	end
+
+	private
+		# Returns content for palindrome testing.
+		def processed_content
+			scan(/[a-z]/i).join.downcase
+		end
 end
